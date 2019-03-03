9d66bc3bd344a35eec2c6850aed919c42ad24339
deepkokje <deepkokje@gmail.com>
2019-03-03 10:13:25 +0530

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,74 @@
-var scores,roundscore,activeplayer,dice;
+var scores,roundscore,activeplayer,dice,gameplaying;
+init();
+
+document.querySelector('.button-roll').addEventListener('click', function (){
+	if(gameplaying){
+
+	 //generate a random numbers
+	 var dice= Math.floor(Math.random() * 6) + 1;
+	
+	 //displaying the result
+		 var diceDOM=document.querySelector('.dice');
+		 diceDOM.style.display='block';
+	  diceDOM.src = 'dice-' + dice + '.png'; // to display the image of dice with corresponding dice number
+	  
+	  //update the round score if it is not 1
+	  if(dice!=1){
+		  //add score
+		  roundscore +=dice;
+		  document.querySelector('#current-' + activeplayer).textContent = roundscore; 
+	  }else{//next player
+	 nextplayer();//DRY principle
+	  }
+
+	}
+
+	
+});
+
+// hold button
+
+document.querySelector('.button-hold').addEventListener('click', function(){
+	if (gameplaying){
+
+	//add current score to global score
+	scores[	activeplayer] += roundscore;
+	//upadte ui
+	document.querySelector('#score-' + activeplayer).textContent= scores[activeplayer];
+					  
+	//check  if player won the game
+	if(scores[activeplayer] >= 20){
+	document.querySelector('#name-' + activeplayer).textContent='Winner!';
+	document.querySelector('.dice').style.display ='none';
+	document.querySelector('.player-' + activeplayer + '-panel').classList.add('winner');
+	document.querySelector('.player-' + activeplayer + '-panel').classList.remove('active');
+	gameplaying = false;
+	}	
+		else {
+			nextplayer();
+		}
+	}
+
+});
+
+function nextplayer(){
+
+	  activeplayer === 0 ? activeplayer = 1 : activeplayer = 0;
+	  roundscore=0;
+	  document.querySelector('#current-0').textContent = '0';
+	   document.querySelector('#current-1').textContent = '0';	
+	 document.querySelector('.player-0-panel').classList.toggle('active');
+	 document.querySelector('.player-1-panel').classList.toggle('active');
+	 document.querySelector('.dice').style.display='none'; //not to display dice=1
+}
+
+document.querySelector('.button-new').addEventListener('click',init);
+
+function init(){
 scores=[0,0];
 roundscore=0;
 activeplayer=0;
+gameplaying = true;
 
 
 //dice= Math.floor(Math.random() * 6) + 1; //for generating random numbers
@@ -15,56 +82,12 @@ document.querySelector('#score-0').textContent='0';     //for setting default va
 document.querySelector('#score-1').textContent='0';    
 document.querySelector('#current-0').textContent='0';
 document.querySelector('#current-1').textContent='0';
-
-document.querySelector('.button-roll').addEventListener('click', function (){
-	 //generate a random numbers
-	var dice= Math.floor(Math.random() * 6) + 1;
-	
-	//displaying the result
-		var diceDOM=document.querySelector('.dice');
-		diceDOM.style.display='block';
-	 diceDOM.src = 'dice-' + dice + '.png'; // to display the image of dice with corresponding dice number
-	 
-	 //update the round score if it is not 1
-	 if(dice!=1){
-		 //add score
-		 roundscore +=dice;
-		 document.querySelector('#current-' + activeplayer).textContent = roundscore; 
-	 }else{//next player
-	nextplayer();//DRY principle
-	 }
-	
-})
-
-// hold button
-
-document.querySelector('.button-hold').addEventListener('click', function(){
-	//add current score to global score
-scores[	activeplayer] += roundscore;
-//upadte ui
-document.querySelector('#score-' + activeplayer).textContent= scores[activeplayer];
-                  
-//check  if player won the game
-if(scores[activeplayer] >= 20){
-document.querySelector('#name-' + activeplayer).textContent='Winner!';
-document.querySelector('.dice').style.display ='none';
-document.querySelector('.player-' + activeplayer + '-panel').classList.add('winner');
-document.querySelector('.player-' + activeplayer + '-panel').classList.remove('active');
-}	
-	else {
-		nextplayer();
-	}
-})
-
-function nextplayer(){
-
-	  activeplayer === 0 ? activeplayer = 1 : activeplayer = 0;
-	  roundscore=0;
-	  document.querySelector('#current-0').textContent = '0';
-	   document.querySelector('#current-1').textContent = '0';	
-	 document.querySelector('.player-0-panel').classList.toggle('active');
-	 document.querySelector('.player-1-panel').classList.toggle('active');
-	 document.querySelector('.dice').style.display='none'; //not to display dice=1
+document.querySelector('#name-0').textContent='Player 1';
+document.querySelector('#name-1').textContent='Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+//document.querySelector('.player-0 -panel').classList.add('active');
+//document.querySelector('.player-1-panel').classList.remove('active');
+//document.querySelector('.player-0 -panel').classList.remove('active');
 }
-
 
